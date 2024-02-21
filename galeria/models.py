8e619d7554f918a88387e43dda0bae070423c072
celorg <marcelinho.ribeirog.2000@gmail.com>
2024-02-21 18:42:47 -0300

    feat: criando um alerta

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 def get_upload_path(instance, filename):
@@ -25,6 +26,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to=get_upload_path, blank=True)
     publicada = models.BooleanField( default= False )
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete= models.SET_NULL,
+        null= True,
+        blank=False,
+        related_name="user",
+    )
 
     def __str__(self):
         return self.nome
